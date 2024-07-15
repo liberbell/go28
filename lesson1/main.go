13a75118c268dc55688601846c644e79e558c0a4
Liberbell <liberbell@gmail.com>
2024-07-15 22:08:29 +0900

    main3

--- lesson1/main.go
+++ lesson1/main.go
@@ -2,10 +2,10 @@ package main
 
 import "fmt"
 
-var x int
-
 func main() {
 	// x = 7
 	// fmt.Printf("%T", x)
-	fmt.Println(x)
+	// fmt.Println(x)
+	xi := []int{2, 4, 7, 9, 42}
+	fmt.Println(xi)
 }
