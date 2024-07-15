2a54f92289aa38c093bc64f054ce81637b7475c5
Liberbell <liberbell@gmail.com>
2024-07-15 22:20:08 +0900

    main11

--- lesson1/main.go
+++ lesson1/main.go
@@ -22,14 +22,14 @@ func main() {
 	// x = 7
 	// fmt.Printf("%T", x)
 	// fmt.Println(x)
-	xi := []int{2, 4, 7, 9, 42}
-	fmt.Println(xi)
+	// xi := []int{2, 4, 7, 9, 42}
+	// fmt.Println(xi)
 
-	m := map[string]int{
-		"Todd": 45,
-		"Job":  42,
-	}
-	fmt.Println(m)
+	// m := map[string]int{
+	// 	"Todd": 45,
+	// 	"Job":  42,
+	// }
+	// fmt.Println(m)
 
 	p1 := person{
 		"Miss",
@@ -37,4 +37,12 @@ func main() {
 	}
 	// fmt.Println(p1)
 	p1.speak()
+
+	sa1 := secretAgent{
+		person{
+			"James",
+			"Bond",
+		},
+		true,
+	}
 }
