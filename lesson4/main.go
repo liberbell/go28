4a2b165157ba30a569363e6dff41445ff8b89784
Liberbell <liberbell@gmail.com>
2024-07-17 08:42:18 +0900

    main2

--- lesson4/main.go
+++ lesson4/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	tpl, err := template.ParseFiles("tpl.gohtml")
+	tpl, err := template.ParseFiles("../lesson3/tpl.gohtml")
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -16,6 +16,7 @@ func main() {
 	if err != nil {
 		log.Println("error creating index.html.")
 	}
+	defer nf.Close()
 
 	err = tpl.Execute(os.Stdout, nil)
 	if err != nil {
