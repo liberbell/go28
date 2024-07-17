be23564aa11e436356c130a9b4f5496886da3cf6
Liberbell <liberbell@gmail.com>
2024-07-18 08:15:40 +0900

    main2

--- lesson5/main.go
+++ lesson5/main.go
@@ -3,10 +3,21 @@ package main
 import (
 	"html/template"
 	"log"
+	"os"
 )
 
 func main() {
 	tpl, err := template.ParseFiles("one.gmao")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.Execute(os.Stdout, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	tpl, err = tpl.ParseFiles("two.gmao", "vespa.gmao")
 	if err != nil {
 		log.Fatalln(err)
 	}
