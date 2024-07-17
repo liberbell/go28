ff5abd4df1508de8552dbd40ef98281b9970ffc9
Liberbell <liberbell@gmail.com>
2024-07-18 08:17:50 +0900

    main3

--- lesson5/main.go
+++ lesson5/main.go
@@ -11,13 +11,20 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-
 	err = tpl.Execute(os.Stdout, nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
 	tpl, err = tpl.ParseFiles("two.gmao", "vespa.gmao")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = tpl.ExecuteTemplate(os.Stdout, "vespa.gmao", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = tpl.Execute(os.Stdout, nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
