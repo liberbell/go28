2a9b810ef126de9f3a33be918b27e63bd82d2ec6
Liberbell <liberbell@gmail.com>
2024-07-19 08:47:47 +0900

    main1

--- lesson5/main.go
+++ lesson5/main.go
@@ -9,7 +9,7 @@ import (
 var tpl *template.Template
 
 func init() {
-	tpl = template.Must(template.ParseFiles("templates/*"))
+	tpl = template.Must(template.ParseGlob("templates/*"))
 }
 
 func main() {
