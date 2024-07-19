8ee2ececdf602911307e8e6563d87a81a45ac33c
Liberbell <liberbell@gmail.com>
2024-07-19 09:36:27 +0900

    tpl1

---
+++ lesson7/variable/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
+}
+
+func main() {
+	sages := []string{"Gandhi", "MLK", "Budda", "Jesus", "Muhammad"}
+	err := tpl.Execute(os.Stdout, sages)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
