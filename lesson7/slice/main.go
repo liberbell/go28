2ef621ea6aed49c2c53c39a8941289c840707408
Liberbell <liberbell@gmail.com>
2024-07-19 09:26:54 +0900

    main2

--- lesson7/slice/main.go
+++ lesson7/slice/main.go
@@ -1,9 +1,17 @@
 package main
 
-import "text/template"
+import (
+	"os"
+	"text/template"
+)
 
 var tpl *template.Template
 
 func init() {
 	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
+}
+
+func main() {
+	sages := []string{"Gandhi", "MLK", "Budda", "Jesus", "Muhammad"}
+	err := tpl.Execute(os.Stdout, sages)
 }
