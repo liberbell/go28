04b6a07522ffdf2bda7e1e4b7951a5efd08b5224
Liberbell <liberbell@gmail.com>
2024-07-22 08:40:22 +0900

    main1

--- lesson8/sample/main.go
+++ lesson8/sample/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "html/template"
+import (
+	"html/template"
+	"os"
+)
 
 func main() {
 	tpl := template.Must(template.New("somthi").Parse("here is the text in the template."))
+	tpl.ExecuteTemplate(os.Stdout, "somethi", nil)
 }
