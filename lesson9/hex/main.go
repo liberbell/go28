9c06e4705a8c942fb0916d6c79d61a09461a6d71
Liberbell <liberbell@gmail.com>
2024-07-22 09:26:43 +0900

    main2

--- lesson9/hex/main.go
+++ lesson9/hex/main.go
@@ -1,9 +1,20 @@
 package main
 
-import "text/template"
+import (
+	"math"
+	"text/template"
+)
 
 var tpl *template.Template
 
 func init() {
 	tpl = template.Must(template.New("").Funcs(fm).ParseFiles("tpl.gohtml"))
+}
+
+func doubule(x int) int {
+	return x + x
+}
+
+func square(x int) float64 {
+	return math.Pow(float64(x), 2)
 }
