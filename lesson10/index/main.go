18beb082882b908b6c16634172f5375ab827e4bb
Liberbell <liberbell@gmail.com>
2024-07-23 08:40:47 +0900

    main2

--- lesson10/index/main.go
+++ lesson10/index/main.go
@@ -3,3 +3,12 @@ package main
 import "text/template"
 
 var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
+}
+
+func main() {
+	xs := []string{"zero", "one", "two", "three", "four", "five"}
+
+}
