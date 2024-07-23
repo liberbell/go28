39834f7bcdee74577a9e05ad8c164c94c3c1443d
Liberbell <liberbell@gmail.com>
2024-07-24 08:09:36 +0900

    main1

--- lesson10/index/main.go
+++ lesson10/index/main.go
@@ -15,6 +15,14 @@ func init() {
 func main() {
 	xs := []string{"zero", "one", "two", "three", "four", "five"}
 
+	data := struct {
+		Words: []string
+		Lname: string
+	}{
+		xs,
+		"Mcleod",
+	}
+
 	err := tpl.Execute(os.Stdout, xs)
 	if err != nil {
 		log.Fatalln(err)
