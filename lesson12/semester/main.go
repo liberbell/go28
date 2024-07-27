1b1e20511a4da232a05af5ae9d35f09d8905ec9c
Liberbell <liberbell@gmail.com>
2024-07-27 11:23:40 +0900

    main6

--- lesson12/semester/main.go
+++ lesson12/semester/main.go
@@ -34,7 +34,7 @@ func main() {
 	y := year{
 		Fall: semester{
 			Term: "Fall",
-			Corses: []course{
+			Courses: []course{
 				course{"CSCI-40", "Introduction to Programming in Go", "4"},
 				course{"CSCI-130", "Introduction to Web Programming with Go", "4"},
 				course{"CSCI-140", "Mobile Apps Using Go", "4"},
