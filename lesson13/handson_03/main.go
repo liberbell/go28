ed4836f08a16eb8483f7a584da160ec568b9da82
Liberbell <liberbell@gmail.com>
2024-07-29 08:34:37 +0900

    main1

---
+++ lesson13/handson_03/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"os"
+)
+
+type hotel struct {
+	Name, Address, City, Zip, Region string
+}
+
+type region struct {
+	Region string
+	Hotels []hotel
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
+}
+
+func main() {
+	h := region{
+		Region: "southern",
+		Hotels: []hotel{
+			hotel{
+				Name:    "Hotel California",
+				Address: "42 Sunset Boulevard",
+				City:    "Los Angeles",
+				Zip:     "95612",
+			},
+			hotel{
+				Name:    "H",
+				Address: "4",
+				City:    "L",
+				Zip:     "95612",
+			},
+		},
+		Region: "nouthern",
+		Hotels: []hotel{
+			hotel{
+				Name:    "Hotel alaska",
+				Address: "someweare cold",
+				City:    "Alaska",
+				Zip:     "99999",
+			},
+			hotel{
+				Name:    "C",
+				Address: "10",
+				City:    "A",
+				Zip:     "99999",
+			},
+		}
+	}
+
+	err := tpl.Execute(os.Stdout, h)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
