e96b3d6980def63837f4d1720aeb74ef1eac6dc4
Liberbell <liberbell@gmail.com>
2024-07-28 19:08:47 +0900

    main2

--- lesson13/handson_02/main.go
+++ lesson13/handson_02/main.go
@@ -3,15 +3,32 @@ package main
 import "html/template"
 
 type hotels struct {
-	Name    string
-	Address string
-	City    string
-	Zipcode int
-	Region  string
+	Name, Address, City, Zipcode, Region string
 }
+
+type hotel []hotels
 
 var tpl *template.Template
 
 func init() {
 	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
+}
+
+func main() {
+	h := hotels{
+		hotel{
+			Name:    "Hotel California",
+			Address: "42 Sunset Boulevard",
+			City:    "Los Angeles",
+			Zip:     "95612",
+			Region:  "southern",
+		},
+		hotel{
+			Name:    "H",
+			Address: "4",
+			City:    "L",
+			Zip:     "95612",
+			Region:  "southern",
+		},
+	}
 }
