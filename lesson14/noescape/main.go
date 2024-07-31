b21c09d32a47b3a141b059e169021ee1ccf7ed56
Liberbell <liberbell@gmail.com>
2024-08-01 08:23:14 +0900

    main3

--- lesson14/noescape/main.go
+++ lesson14/noescape/main.go
@@ -13,3 +13,11 @@ var tpl *template.Template
 func init() {
 	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
 }
+
+func main() {
+	home = Page{
+		Title:   "Nothin escape",
+		Heading: "Nothing escaped with text/template",
+		Input:   `<script>alert("YoW");</script>`,
+	}
+}
