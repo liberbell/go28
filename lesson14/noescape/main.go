87484d2fe9657aaf9e61948d1cf13478076b9332
Liberbell <liberbell@gmail.com>
2024-08-01 08:21:14 +0900

    main1

---
+++ lesson14/noescape/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "html/template"
+
+type Page struct {
+	Title   string
+	Heading string
+	Input   string
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
+}
