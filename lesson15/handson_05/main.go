e4e902b909b9b061b33cde025bb93b26053e55dc
Liberbell <liberbell@gmail.com>
2024-08-03 15:35:37 +0900

    main4

--- lesson15/handson_05/main.go
+++ lesson15/handson_05/main.go
@@ -33,4 +33,11 @@ func handle(conn net.Conn) {
 
 func rot13(bs []byte) []byte {
 	var r13 = make([]byte, len(bs))
+	for i, v := range bs {
+		if v <= 109 {
+			r13[i] = v + 13
+		} else {
+			r13[i] = v - 13
+		}
+	}
 }
