5458ca9063a3c5c71622dd7fb209efc8d5da496d
Liberbell <liberbell@gmail.com>
2024-08-05 08:12:12 +0900

    main2

--- lesson16/handson_03/main.go
+++ lesson16/handson_03/main.go
@@ -3,9 +3,27 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"net"
 	"strings"
 )
+
+func main() {
+	li, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Fatalln(err.Error())
+			continue
+		}
+		go handle(conn)
+	}
+}
 
 func handle(conn net.Conn) {
 	defer conn.Close()
