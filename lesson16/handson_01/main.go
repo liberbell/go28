6e86590dbf9ecd7b90fe2ddbafb3e5405c72341d
Liberbell <liberbell@gmail.com>
2024-08-04 14:16:20 +0900

    main4

--- lesson16/handson_01/main.go
+++ lesson16/handson_01/main.go
@@ -31,3 +31,10 @@ func request(conn net.Conn) {
 		i++
 	}
 }
+
+func respond(conn net.Conn) {
+	body := `<!DOCTYPE html><html lang="en"><head><meta charset"utf-8"><title></title></head><body><strong>Hello</strong></body>`
+
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "content-length: %d\r\n", len(body))
+}
