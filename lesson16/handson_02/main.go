43288850d71e0ebe450576820fb3190ab214b7e4
Liberbell <liberbell@gmail.com>
2024-08-04 14:33:28 +0900

    main2

--- lesson16/handson_02/main.go
+++ lesson16/handson_02/main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"strings"
 )
+
+func handle(conn net.Conn) {
+	defer conn.Close()
+
+	request(conn)
+
+	respond(conn)
+}
+
+func request(conn net.Conn) {
+	i := 0
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		if i == 0 {
+			m := strings.Fields(ln)[0]
+			fmt.Println("***Method", m)
+		}
+		if ln == "" {
+			break
+		}
+		i++
+	}
+}
 
 func respond(conn net.Conn) {
 	body := `<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><title></title></head><body><strong>Hello world</strong></body></html>`
