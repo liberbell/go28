3404d17d32aff56a9a7e7292ef9d5ba2b975fdd4
Liberbell <liberbell@gmail.com>
2024-08-05 08:29:43 +0900

    main4

--- lesson16/handson_04/main.go
+++ lesson16/handson_04/main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"net"
 )
 
 func handle(conn net.Conn) {
 	defer conn.Close()
 	request(conn)
+}
+
+func request(conn net.Conn) {
+	i := 0
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+
+	}
 }
