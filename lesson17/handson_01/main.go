cfc95b5e6a27b2862538c56cb11feb69b8e3c83c
Liberbell <liberbell@gmail.com>
2024-08-06 08:28:24 +0900

    main2

--- lesson17/handson_01/main.go
+++ lesson17/handson_01/main.go
@@ -8,5 +8,9 @@ import (
 type hotdog int
 
 func (m hotdog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Println()
+	fmt.Println("Any code you want in this func")
+}
+
+func main() {
+
 }
