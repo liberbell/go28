091b8e2ac617f585ace945f5f7bd5f79cb23053d
Liberbell <liberbell@gmail.com>
2024-08-07 09:09:44 +0900

    main1

---
+++ lesson17/handson_04/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+type hotdog int
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("index.gohtml"))
+}
+
+func (m hotdog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	data := struct {
+		Method     string
+		Submission url.Values
+	}{
+		r.Method,
+		r.Form,
+	}
+	tpl.ExecuteTemplate(w, "index.gohtml", data)
+}
+
+func main() {
+	var d hotdog
+	http.ListenAndServe(":8080", d)
+}
