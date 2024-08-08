e6ac9d753eb80ede9b6652472450fec5621ce3fd
Liberbell <liberbell@gmail.com>
2024-08-08 09:16:19 +0900

    main2

--- lesson17/handson_08/main.go
+++ lesson17/handson_08/main.go
@@ -12,3 +12,8 @@ func (m hotdog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprintf(w, "<h1>Any code you want in this func</h1>")
 }
+
+func main() {
+	var d hotdog
+	http.ListenAndServe(":8080", d)
+}
