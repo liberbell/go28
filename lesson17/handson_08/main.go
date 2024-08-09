ade1569c39ef55363319d1e3d610f3e8f14a6833
Liberbell <liberbell@gmail.com>
2024-08-09 13:08:03 +0900

    main1

--- lesson17/handson_08/main.go
+++ lesson17/handson_08/main.go
@@ -9,7 +9,7 @@ type hotdog int
 
 func (m hotdog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Original-key", "This is an original key")
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Content-Type", "text/plane; charset=utf-8")
 	fmt.Fprintf(w, "<h1>Any code you want in this func</h1>")
 }
 
