5e4399c41259d11caf069d9b3d1bcdd6b7e0c810
Liberbell <liberbell@gmail.com>
2024-08-09 20:51:23 +0900

    main1

---
+++ lesson18/handson_03/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func d(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "doggy doggy doggy")
+}
+
+func c(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "kitty kitty kitty")
+}
+
+func main() {
+	http.Handle("/dog/", http.HandlerFunc(d))
+	http.Handle("/cat", http.HandlerFunc(c))
+	http.ListenAndServe(":8080", nil)
+}
