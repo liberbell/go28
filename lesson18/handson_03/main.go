1db555a0d43d766e6b837c800018a963d26f4587
Liberbell <liberbell@gmail.com>
2024-08-10 14:04:43 +0900

    main1

--- lesson18/handson_03/main.go
+++ lesson18/handson_03/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"html/template"
+	"net/http"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -15,4 +16,15 @@ func init() {
 func main() {
 	mux := httprouter.New()
 	mux.GET("/", index)
+}
+
+func index(w http.ResponseWriter, r *http.Request)  {
+	err := tpl.ExecuteTemplate(w, "index.gohtml", nil)
+	Handleerr
+}
+
+func HandleError(w http.ResponseWriter, err error)  {
+	if err != nil {
+		http.Error(w err.Error(), )
+	}
 }
