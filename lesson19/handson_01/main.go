4f6ef5931ee90a8880c1f252f2b739a6654453fd
Liberbell <liberbell@gmail.com>
2024-08-11 11:41:15 +0900

    main2

--- lesson19/handson_01/main.go
+++ lesson19/handson_01/main.go
@@ -1,10 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"io"
+	"net/http"
+)
 
 func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/dog", dog)
 	http.HandleFunc("/cat", cat)
 
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "this is an index.")
+}
+
+func dog(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "doggy doggy doggy")
 }
