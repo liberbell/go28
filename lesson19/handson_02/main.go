d7f858a84623eacbbbcd9bb4df595e2a94184753
Liberbell <liberbell@gmail.com>
2024-08-11 11:49:12 +0900

    main1

---
+++ lesson19/handson_02/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/dog/", dog)
+	http.HandleFunc("/cat/", cat)
+	http.HandleFunc("/data/", data)
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "this is an index.")
+}
+
+func dog(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "doggy doggy doggy")
+}
+
+func cat(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "kitty kitty kitty")
+}
+
+func data(w http.ResponseWriter, r *http.Request) {
+	tpl, err := template.ParseFiles("data.gohtml")
+	if err != nil {
+		log.Fatalln("parse error", err)
+	}
+}
