525359d781de7114536de7d324ceab3588d1036d
Liberbell <liberbell@gmail.com>
2024-08-12 11:49:15 +0900

    main1

---
+++ lesson19/handson_04/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer lis.Close()
+
+	for {
+		conn, err := lis.Accept()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+}
