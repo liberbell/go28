6e0f0d6161d928f7b85e4f96dbbe14b35adf0c93
Liberbell <liberbell@gmail.com>
2024-08-12 12:40:54 +0900

    main1

---
+++ lesson19/handson_07/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer lis.Close()
+
+	for {
+		conn, err := lis.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		scanner := bufio.NewScanner(conn)
+		for scanner.Scan() {
+			ln := scanner.Text()
+			fmt.Println(ln)
+			if ln == "" {
+				fmt.Println("This is the end of the HTTP request Headers")
+				break
+			}
+		}
+
+		fmt.Println("Code is here")
+		io.WriteString(conn, "I see you connected.")
+		conn.Close()
+	}
+
+}
