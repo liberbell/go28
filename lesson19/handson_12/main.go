6ec967022be2724fb9b558bc5be0e441bb110a3d
Liberbell <liberbell@gmail.com>
2024-08-14 08:12:31 +0900

    main2

--- lesson19/handson_12/main.go
+++ lesson19/handson_12/main.go
@@ -48,7 +48,11 @@ func serve(c net.Conn) {
 		i++
 	}
 	body := "CHECK OUT THE RESPONSE BODY PAYLOAD"
-	io.WriteString(c, "HTTP/1.1 200 \r\n")
+	body += "\n"
+	body += rMethod
+	body += "\n"
+	body += rURL
+	io.WriteString(c, "HTTP/1.1 200 OK\r\n")
 	fmt.Fprintf(c, "Content-Length: %d\r\n", len(body))
 	fmt.Fprint(c, "Content-Type: text/plain\r\n")
 	io.WriteString(c, "\r\n")
