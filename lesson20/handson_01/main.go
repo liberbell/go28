71d5e13415008297f828bd0afad1b60133e57039
Liberbell <liberbell@gmail.com>
2024-08-14 08:33:03 +0900

    main2

--- lesson20/handson_01/main.go
+++ lesson20/handson_01/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "net/http"
+import (
+	"io"
+	"net/http"
+)
 
 func dog(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, `
+	<!--not serving from our server-->
+	<img src="https://upload.wikimedia.org/wikipedia/commons/6/6e/Golde33443.jpg">
+	`)
 }
