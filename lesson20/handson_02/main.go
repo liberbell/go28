9feba4dddd6de045cb99f083e6903f95dc90a9ff
Liberbell <liberbell@gmail.com>
2024-08-14 08:58:28 +0900

    main3

--- lesson20/handson_02/main.go
+++ lesson20/handson_02/main.go
@@ -15,11 +15,14 @@ func main() {
 func dog(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	io.WriteString(w, `
-	<!--not serving from our server-->
-	<img src="https://upload.wikimedia.org/wikipedia/commons/6/6e/Golde33443.jpg">
+	<img src="toby.jpg">
 	`)
 }
 
-func dogPic(w http.ResponseWriter, r *http.Request)  {
-	f. err := os.Open("toby.jpg")
+func dogPic(w http.ResponseWriter, r *http.Request) {
+	f, err := os.Open("toby.jpg")
+	if err != nil {
+		http.Error(w, "file not found", 404)
+	}
+	defer f.Close()
 }
