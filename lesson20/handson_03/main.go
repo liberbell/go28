bfb71a3f7698303a01fc580b643a8e6745111715
Liberbell <liberbell@gmail.com>
2024-08-15 08:56:05 +0900

    main3

--- lesson20/handson_03/main.go
+++ lesson20/handson_03/main.go
@@ -13,7 +13,7 @@ func main() {
 
 func dog(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	io.WriteString(w, `<img src="toby.jpg">`)
+	io.WriteString(w, `<img src="toby.jpg"> `)
 }
 
 func dogPic(w http.ResponseWriter, r *http.Request) {
