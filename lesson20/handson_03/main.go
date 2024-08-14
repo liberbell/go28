b750d47ad2ef660d179147ae2d4f7c7ae7b22421
Liberbell <liberbell@gmail.com>
2024-08-15 08:01:07 +0900

    main2

--- lesson20/handson_03/main.go
+++ lesson20/handson_03/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "net/http"
+import (
+	"io"
+	"net/http"
+)
 
 func dog(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, `<img src="toby.jpg">`)
+}
+
+func dogPic(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "toby.jpg")
 }
