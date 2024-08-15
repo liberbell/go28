440f339dc5744ea7a238c2cfb00c5ee9f0221853
Liberbell <liberbell@gmail.com>
2024-08-16 08:07:58 +0900

    main1

--- lesson20/handson_06/main.go
+++ lesson20/handson_06/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "net/http"
+import (
+	"io"
+	"net/http"
+)
 
 func main() {
 	http.ListenAndServe(":8080", http.FileServer(http.Dir(".")))
+	http.HandleFunc("/dog", dog)
+	http.ListenAndServe(":8080", nil)
+}
+
+func dog(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, `<img src="toby.jpg>`)
 }
