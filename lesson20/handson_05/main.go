142dc956488fe890bc235d3049083a9969f5079e
Liberbell <liberbell@gmail.com>
2024-08-15 19:13:26 +0900

    main3

--- lesson20/handson_05/main.go
+++ lesson20/handson_05/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", dog)
-	http.Handle("/resources", http.StripPrefix("/resources", http.FileServer(http.Dir("./assets"))))
+	http.Handle("/resources/", http.StripPrefix("/resources", http.FileServer(http.Dir("./assets"))))
 	http.ListenAndServe(":8080", nil)
 }
 
