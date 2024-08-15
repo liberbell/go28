0738706346807cb44db48bc4bba07c2fa7065808
Liberbell <liberbell@gmail.com>
2024-08-15 19:12:17 +0900

    main2

--- lesson20/handson_05/main.go
+++ lesson20/handson_05/main.go
@@ -7,11 +7,11 @@ import (
 
 func main() {
 	http.HandleFunc("/", dog)
-	http.Handle("/resources", http.StripPrefix("/resources"))
+	http.Handle("/resources", http.StripPrefix("/resources", http.FileServer(http.Dir("./assets"))))
 	http.ListenAndServe(":8080", nil)
 }
 
 func dog(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	io.WriteString(w, `<img src="toby.jpg">`)
+	io.WriteString(w, `<img src="/resources/toby.jpg">`)
 }
