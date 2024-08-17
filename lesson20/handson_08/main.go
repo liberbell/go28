cff67c59ec619c72b0a69577d48e11c1f19982c2
Liberbell <liberbell@gmail.com>
2024-08-17 17:47:53 +0900

    main4

--- lesson20/handson_08/main.go
+++ lesson20/handson_08/main.go
@@ -15,6 +15,8 @@ func init() {
 func main() {
 	fs := http.FileServer(http.Dir("public"))
 	http.Handle("/pics", fs)
+	http.HandleFunc("/", dogs)
+	http.ListenAndServe(":8080", nil)
 }
 
 func dogs(w http.ResponseWriter, r *http.Request) {
