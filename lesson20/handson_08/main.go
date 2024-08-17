c58e52506e5abb8e89786e081247767c0196a651
Liberbell <liberbell@gmail.com>
2024-08-17 17:52:56 +0900

    main5

--- lesson20/handson_08/main.go
+++ lesson20/handson_08/main.go
@@ -14,7 +14,7 @@ func init() {
 
 func main() {
 	fs := http.FileServer(http.Dir("public"))
-	http.Handle("/pics", fs)
+	http.Handle("/pics/", fs)
 	http.HandleFunc("/", dogs)
 	http.ListenAndServe(":8080", nil)
 }
