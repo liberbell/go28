c974b564ca115739fe14134b6833bb47fbdaf172
Liberbell <liberbell@gmail.com>
2024-08-18 20:01:22 +0900

    main3

--- lesson20/handson_10/main.go
+++ lesson20/handson_10/main.go
@@ -12,13 +12,14 @@ func init() {
 	tpl = template.Must(template.ParseFiles("templates/index.gohtml"))
 }
 
-func main()  {
+func main() {
 	http.HandleFunc("/", dogs)
+	http.Handle("/resources/", http.StripPrefix("/resources", http.FileServer(http.Dir("."))))
 }
 
-func dogs(w http.ResponseWriter, r *http.Request)  {
-	err := tpl.Execute(w nil)
+func dogs(w http.ResponseWriter, r *http.Request) {
+	err := tpl.Execute(w, nil)
 	if err != nil {
-		log.Fatalln("Template did't execute.: " err)
+		log.Fatalln("Template did't execute.: ", err)
 	}
 }
