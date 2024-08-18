a9a6ee3e8d7ac18742c9c28aa10793588d11ebca
Liberbell <liberbell@gmail.com>
2024-08-18 19:59:42 +0900

    main2

--- lesson20/handson_10/main.go
+++ lesson20/handson_10/main.go
@@ -1,10 +1,24 @@
 package main
 
-import "text/template"
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
 
 var tpl *template.Template
 
-func init()  {
-	tpl = template.Must(template.ParseFiles("templates/index.gohtml
-	"))
+func init() {
+	tpl = template.Must(template.ParseFiles("templates/index.gohtml"))
+}
+
+func main()  {
+	http.HandleFunc("/", dogs)
+}
+
+func dogs(w http.ResponseWriter, r *http.Request)  {
+	err := tpl.Execute(w nil)
+	if err != nil {
+		log.Fatalln("Template did't execute.: " err)
+	}
 }
