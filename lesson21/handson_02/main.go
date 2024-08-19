e7c4d30ac7586aa0ddfc6832806ded05d0f4f5a4
Liberbell <liberbell@gmail.com>
2024-08-19 23:54:01 +0900

    main1

---
+++ lesson21/handson_02/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	v := r.FormValue("q")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, "do my search: "+v)
+}
