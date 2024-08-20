5d41048a99e82acc7082edb64cd6f01ba78d249d
Liberbell <liberbell@gmail.com>
2024-08-20 23:27:45 +0900

    main4

--- lesson21/handson_03/main.go
+++ lesson21/handson_03/main.go
@@ -18,6 +18,12 @@ type person struct {
 	Subscribed bool
 }
 
+func main() {
+	http.HandleFunc("/", foo)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
 func foo(w http.ResponseWriter, r *http.Request) {
 	f := r.FormValue("first")
 	l := r.FormValue("last")
