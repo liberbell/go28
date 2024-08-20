84e6b3c3bffcf4df17b505940999367f56eca7d4
Liberbell <liberbell@gmail.com>
2024-08-20 22:59:55 +0900

    main1

--- lesson21/handson_03/main.go
+++ lesson21/handson_03/main.go
@@ -1,9 +1,24 @@
 package main
 
-import "html/template"
+import (
+	"html/template"
+	"net/http"
+)
 
 var tpl *template.Template
 
 func init() {
 	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+type person struct {
+	FirstName  string
+	LastName   string
+	Subscribed bool
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	f := r.FormValue("first")
+	l := r.FormValue("last")
+	s := r.FormValue("subscribed") == "on"
 }
