b389d1c51e5a14fd6f8622c50d7ab0c9d0c68323
Liberbell <liberbell@gmail.com>
2024-08-21 23:16:37 +0900

    main1

---
+++ lesson21/handson_05/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	var s string
+	fmt.Println(r.Method)
+	if r.Method == http.MethodPost {
+		f, h, err := r.FormFile("q")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer f.Close()
+
+		fmt.Println("\nfile: ", f, "\nheader: ", h, "\nerr", err)
+
+		bs, err := ioutil.ReadAll(f)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		s = string(bs)
+
+		dst, err := os.Create(filepath.Join("./user", h.Filename))
+	}
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, `
+	<form method="POST" enctype="multipart/form-data">
+	<input type="file" name="q">
+	<input type="submit">
+	</form>
+	<br>`+s)
+}
