02e11a6895a46e3afbf14dc42897ae9bea2f39c5
Liberbell <liberbell@gmail.com>
2024-08-21 22:56:04 +0900

    main2

--- lesson21/handson_04/main.go
+++ lesson21/handson_04/main.go
@@ -8,4 +8,11 @@ import (
 func foo(w http.ResponseWriter, r *http.Request) {
 	var s string
 	fmt.Println(r.Method)
+	if r.Method == http.MethodPost {
+		f, h, err := r.FormFile("q")
+		if err != nil {
+			http.Error(w, err.Error())
+			return
+		}
+	}
 }
