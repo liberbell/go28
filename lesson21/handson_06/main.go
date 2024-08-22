ba2279a81c3a367c1b495c412526143300b295c5
Liberbell <liberbell@gmail.com>
2024-08-22 23:09:14 +0900

    main2

--- lesson21/handson_06/main.go
+++ lesson21/handson_06/main.go
@@ -15,6 +15,12 @@ func init() {
 	tpl = template.Must(template.ParseGlob("templates/*"))
 }
 
+type person struct {
+	FirstName  string
+	LastName   string
+	Subscribed bool
+}
+
 func main() {
 	http.HandleFunc("/", foo)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
