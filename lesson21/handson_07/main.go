3fecf40f510f87553e84267551ae0f1e2f59c279
Liberbell <liberbell@gmail.com>
2024-08-23 23:09:02 +0900

    main1

---
+++ lesson21/handson_07/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+type person struct {
+	FirstName  string
+	LastName   string
+	Subscribed bool
+}
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	bs := make([]byte, r.ContentLength)
+	r.Body.Read(bs)
+	body := string(bs)
+
+	err := tpl.ExecuteTemplate(w, "index.gohtml", body)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		log.Fatalln(err)
+	}
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Your request method at bar: ", r.Method)
+}
