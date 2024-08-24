65c0d79a88896a1103b84b5e3bb7c4735578dd34
Liberbell <liberbell@gmail.com>
2024-08-24 13:06:11 +0900

    main1

--- lesson21/handson_07/main.go
+++ lesson21/handson_07/main.go
@@ -21,7 +21,7 @@ type person struct {
 func main() {
 	http.HandleFunc("/", foo)
 	http.HandleFunc("/bar", bar)
-	http.HandleFunc("/barred", barred)
+	// http.HandleFunc("/barred", barred)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":8080", nil)
 }
@@ -37,7 +37,7 @@ func bar(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/", http.StatusMovedPermanently)
 }
 
-func barred(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("your request method at barred: ", r.Method)
-	tpl.ExecuteTemplate(w, "index.gohtml", nil)
-}
+// func barred(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Println("your request method at barred: ", r.Method)
+// 	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+// }
