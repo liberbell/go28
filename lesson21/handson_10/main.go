f795e61d0e657fc9213ad6507b845025d8488e3f
Liberbell <liberbell@gmail.com>
2024-08-25 13:39:15 +0900

    main2

--- lesson21/handson_10/main.go
+++ lesson21/handson_10/main.go
@@ -1,13 +1,19 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"strconv"
+)
 
 func foo(w http.ResponseWriter, r *http.Request) {
 	cookie, err := r.Cookie("my-cookie")
-	if err != nil {
+	if err == http.ErrNoCookie {
 		cookie = &http.Cookie{
 			Name:  "my-cookie",
 			Value: "0",
 		}
 	}
+	count, _ := strconv.Atoi(cookie.Value)
+	count++
+	cookie.Value = strconv.Itoa(count)
 }
