64d2112d34509f991910d18d5e3c44dc2c82a858
Liberbell <liberbell@gmail.com>
2024-08-25 13:29:57 +0900

    main5

--- lesson21/handson_09/main.go
+++ lesson21/handson_09/main.go
@@ -18,6 +18,7 @@ func set(w http.ResponseWriter, r *http.Request) {
 	http.SetCookie(w, &http.Cookie{
 		Name:  "my-cookie",
 		Value: "some value",
+		Path:  "/",
 	})
 	fmt.Fprintln(w, "Cookie Written - Check your Browser")
 	fmt.Fprintln(w, "in chrome go to: dev tools / applications / cookies")
