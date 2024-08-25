b4ab8d9a213b6eac78681e8d8445bbc3499f2b92
Liberbell <liberbell@gmail.com>
2024-08-26 08:52:21 +0900

    main2

--- lesson22/handson_01/main.go
+++ lesson22/handson_01/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	uuid "github.com/satori/go.uuid"
+)
 
 func foo(w http.ResponseWriter, r *http.Request) {
-
+	cookie, err := r.Cookie("session-id")
+	if err != nil {
+		id := uuid.NewV4()
+		cookie = &http.Cookie{
+			Name: "session-id",
+		}
+	}
 }
