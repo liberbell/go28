75fdb86a0080d3a61fb480423dede0217c50d651
Liberbell <liberbell@gmail.com>
2024-08-25 13:52:25 +0900

    main2

--- lesson21/handson_11/main.go
+++ lesson21/handson_11/main.go
@@ -8,3 +8,10 @@ import (
 func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, `<h1><a href="/set">set a cookie</a></h1>`)
 }
+
+func set(w http.ResponseWriter, r *http.Request) {
+	http.SetCookie(w, &http.Cookie{
+		Name:  "my-cookie",
+		Value: "some value",
+	})
+}
