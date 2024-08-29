24216e62bdaf4570b2c3c020cfe4ebe56e8fc370
Liberbell <liberbell@gmail.com>
2024-08-30 08:36:38 +0900

    main1

---
+++ lesson23/handson_02 copy/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+type user struct {
+	UserName string
+	Password string
+	First    string
+	Last     string
+}
+
+var tpl *template.Template
+var dbUsers = map[string]user{}
+var dbSessions = map[string]string{}
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/bar", bar)
+	http.HandleFunc("/signup", signup)
+	http.Handle("favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	u := getUser(r)
+	tpl.ExecuteTemplate(w, "index.gohtml", u)
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
+	u := getUser(r)
+	if !alreadyLoggedIn(r) {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	tpl.ExecuteTemplate(w, "bar.gohtml", u)
+}
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	if alreadyLoggedIn(r) {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
+
+	if r.Method == http.MethodPost {
+		un := r.FormValue("username")
+		p := r.FormValue("password")
+		f := r.FormValue("firstname")
+		l := r.FormValue("lastname")
+
+		if _, ok := dbUsers[un]; ok {
+			http.Error(w, "Username already taken.", http.StatusForbidden)
+			return
+		}
+
+		sID, _ := uuid.NewV4()
+		c := &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+		http.SetCookie(w, c)
+		dbSessions[c.Value] = un
+
+		u := user{un, p, f, l}
+		dbUsers[un] = u
+
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	tpl.ExecuteTemplate(w, "signup.gohtml", nil)
+}
