e06f30a53e5836cefa02bd7769954051d595211e
Liberbell <liberbell@gmail.com>
2024-09-03 08:55:16 +0900

    main5

--- lesson24/handson_01/main.go
+++ lesson24/handson_01/main.go
@@ -27,6 +27,11 @@ func getCookie(w http.ResponseWriter, r *http.Request) *http.Cookie {
 	c, err := r.Cookie("session")
 	if err != nil {
 		sID, _ := uuid.NewV4
-
+		c = &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+		http.SetCookie(w, c)
 	}
+	return c
 }
