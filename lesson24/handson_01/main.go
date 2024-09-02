f32e66885fdf91c8038b9da8f0af2e85c4b45872
Liberbell <liberbell@gmail.com>
2024-09-03 08:46:26 +0900

    main2

--- lesson24/handson_01/main.go
+++ lesson24/handson_01/main.go
@@ -1,9 +1,20 @@
 package main
 
-import "html/template"
+import (
+	"html/template"
+	"net/http"
+)
 
 var tpl *template.Template
 
 func init() {
 	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
 }
