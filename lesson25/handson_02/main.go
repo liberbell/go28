651963dc5a26d62c6e77fb1a9b6ff56b75ddc5de
Liberbell <liberbell@gmail.com>
2024-09-06 08:43:13 +0900

    main2

--- lesson25/handson_02/main.go
+++ lesson25/handson_02/main.go
@@ -1,10 +1,18 @@
 package main
 
-import "encoding/base64"
+import (
+	"encoding/base64"
+	"fmt"
+)
 
 func main() {
 	s := "Lately, I've been, I've been losin' sleep Dreamin' about the things that we could be But baby, I've been, I've been prayin' hard Said, No more countin' dollars, we'll be countin' stars Yeah, we'll be countin' stars"
 
 	encodeStd := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 	s64 := base64.NewEncoding(encodeStd).EncodeToString([]byte(s))
+
+	fmt.Println(len(s))
+	fmt.Println(len(s64))
+	fmt.Println(s)
+	fmt.Println(s64)
 }
