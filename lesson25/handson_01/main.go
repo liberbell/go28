8e14b665e1c211d639e81cdb56388ed8e9248f57
Liberbell <liberbell@gmail.com>
2024-09-05 09:24:16 +0900

    main3

--- lesson25/handson_01/main.go
+++ lesson25/handson_01/main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"crypto/hmac"
 	"crypto/sha256"
+	"fmt"
+	"io"
 )
 
 func getCode(s string) string {
 	h := hmac.New(sha256.New, []byte("ourkey"))
+	io.WriteString(h, s)
+	return fmt.Sprintf("%s", h.Sum(nil))
 }
