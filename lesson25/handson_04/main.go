a241083cd0893af59eb30510df4ff76ebc244292
Liberbell <liberbell@gmail.com>
2024-09-06 09:20:09 +0900

    main2

--- lesson25/handson_04/main.go
+++ lesson25/handson_04/main.go
@@ -6,6 +6,12 @@ import (
 	"net/http"
 )
 
+func main() {
+	http.HandleFunc("/", foo)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
 func foo(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	log.Println(ctx)
