99cad16c7bbe606486b7a57238238233cbaabd24
Liberbell <liberbell@gmail.com>
2024-09-07 10:51:09 +0900

    main3

--- lesson25/handson_05/main.go
+++ lesson25/handson_05/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 )
 
@@ -9,4 +10,12 @@ func foo(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	ctx = context.WithValue(ctx, "userID", 007)
 	ctx = context.WithValue(ctx, "fname", "James")
+
+	result := dbAcces(ctx)
+	fmt.Fprintln(w, result)
+}
+
+func dbAcces(ctx context.Context) int {
+	uid := ctx.Value("userID").(int)
+	return uid
 }
