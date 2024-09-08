b8582ff028d1d931331809876ddb3a9d1ef4acef
Liberbell <liberbell@gmail.com>
2024-09-08 15:45:53 +0900

    main2

--- lesson25/handson_09/main.go
+++ lesson25/handson_09/main.go
@@ -1,7 +1,17 @@
 package main
 
+import "net/http"
+
 type person struct {
 	Fname string
 	Lname string
 	Items []string
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
+	s := `<!DOCTYPE html>
+	      <head>
+		  <meta charset="utf-8">
+		  <title>F00</title>
+		  `
 }
