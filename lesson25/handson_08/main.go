cef4298750aaf98d79cae92115e1cea51c39c46e
Liberbell <liberbell@gmail.com>
2024-09-08 15:16:36 +0900

    main2

--- lesson25/handson_08/main.go
+++ lesson25/handson_08/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"context"
+)
+
+func gen(ctx context.Context) <-chan int {
+	ch := make(chan int)
+	go func() {
+		var n int
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case ch <- n:
+				n++
+			}
+		}
+	}()
+	return ch
+}
