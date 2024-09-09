ebf0fb14aa3f55355cef43d1834861e497530253
Liberbell <liberbell@gmail.com>
2024-09-10 08:43:01 +0900

    main3

--- lesson25/handson_11/main.go
+++ lesson25/handson_11/main.go
@@ -1,8 +1,17 @@
 package main
 
+import "encoding/json"
+
 type city struct {
 	Latitude, Longitude float64
 	City                string
 }
 
 type cities []city
+
+func main() {
+	var data cities
+	rcvd := `[{"precision":"zip","Latitude":37.7668,"Longitude":-122.3959,"Address":"","City":"SAN FRANCISCO","State":"CA","Zip":"94107","Country":"US"},{"precision":"zip","Latitude":37.371991,"Longitude":-122.02602,"Address":"","City":"SUNNYVALE","State":"CA","Zip":"94085","Country":"US"}]`
+
+	err := json.Unmarshal([]byte(rcvd), &data)
+}
