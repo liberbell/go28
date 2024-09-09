c5a681c0ffd07772f03d2b738185451258649b88
Liberbell <liberbell@gmail.com>
2024-09-10 08:28:52 +0900

    main2

--- lesson25/handson_10/main.go
+++ lesson25/handson_10/main.go
@@ -1,7 +1,20 @@
 package main
 
+type thumbnail struct {
+	URL           string
+	Height, Width int
+}
+
 type img struct {
 	Width, Height int
 	Title         string
 	Thumbnail     thumbnail
+	Animated      bool
+	IDs           []int
+}
+
+func main() {
+	var data img
+	rcvd := `{"Width":800,"Height":600,"Title":"View from 15th Floor","Thumbnail":{"Url":"http://www.example.com/image/481989943","Height":125,"Width":100},"Animated":false,"IDs":[116,943,234,38793]}`
+
 }
