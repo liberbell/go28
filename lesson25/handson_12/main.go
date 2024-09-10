c80e5f0359506f3fa59d570a1a758e93c7e893c8
Liberbell <liberbell@gmail.com>
2024-09-11 08:32:45 +0900

    main3

--- lesson25/handson_12/main.go
+++ lesson25/handson_12/main.go
@@ -6,4 +6,7 @@ type city struct {
 	Maui       float64 `json:"Longitude"`
 	Java       string  `json:"Address"`
 	NewZealand string  `Json:"City"`
+	Skye       string  `Json:"State"`
+	Oahu       string  `json:"Zip"`
+	Hawaii     string  `Json:"Country"`
 }
