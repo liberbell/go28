3d78d58594d34f58909f8720c82c42421f217694
Liberbell <liberbell@gmail.com>
2024-09-11 08:31:28 +0900

    main2

--- lesson25/handson_12/main.go
+++ lesson25/handson_12/main.go
@@ -1,6 +1,9 @@
 package main
 
 type city struct {
-	Bali  string  `json:"Postal"`
-	Kauai float64 `json:"Latitude"`
+	Bali       string  `json:"Postal"`
+	Kauai      float64 `json:"Latitude"`
+	Maui       float64 `json:"Longitude"`
+	Java       string  `json:"Address"`
+	NewZealand string  `Json:"City"`
 }
