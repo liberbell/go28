5a5ef564378469c2fe21449368a368444ad0bad5
Liberbell <liberbell@gmail.com>
2024-09-11 08:34:22 +0900

    main4

--- lesson25/handson_12/main.go
+++ lesson25/handson_12/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "encoding/json"
+
 type city struct {
 	Bali       string  `json:"Postal"`
 	Kauai      float64 `json:"Latitude"`
@@ -9,4 +11,13 @@ type city struct {
 	Skye       string  `Json:"State"`
 	Oahu       string  `json:"Zip"`
 	Hawaii     string  `Json:"Country"`
+}
+
+type cities []city
+
+func main() {
+	var data cities
+
+	rcvd := `[{"Postal":"zip","Latitude":37.7668,"Longitude":-122.3959,"Address":"","City":"SAN FRANCISCO","State":"CA","Zip":"94107","Country":"US"},{"Postal":"zip","Latitude":37.371991,"Longitude":-122.02602,"Address":"","City":"SUNNYVALE","State":"CA","Zip":"94085","Country":"US"}]`
+	err := json.Unmarshal([]byte(rcvd), &data)
 }
