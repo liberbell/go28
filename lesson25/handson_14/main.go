ac6c9b27f7cb1cca754d49247711dee5ac42fcde
Liberbell <liberbell@gmail.com>
2024-09-12 08:27:19 +0900

    main2

--- lesson25/handson_14/main.go
+++ lesson25/handson_14/main.go
@@ -1,6 +1,19 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 type model struct {
 	State    bool
 	Pictures []string
+}
+
+func main() {
+	m := model{}
+	bs, err := json.Marshal(m)
+	if err != nil {
+		fmt.Println("error: ", err)
+	}
 }
