63d5b5fddfcd0e9d3d80dc58528aa89b721bb257
Liberbell <liberbell@gmail.com>
2024-09-16 09:32:53 +0900

    main2

--- lesson26/handson_03/main.go
+++ lesson26/handson_03/main.go
@@ -1,9 +1,22 @@
 package main
 
-import "text/template"
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
 
 var tpl *template.Template
 
 func init() {
 	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	s := `<h1>Here is some text from foo`
+	fmt.Fprintln(w, s)
 }
