c1a9e3e184c7041908d43a700a0280e061c4e255
Liberbell <liberbell@gmail.com>
2024-09-18 08:27:30 +0900

    main2

--- lesson27/handson_01/main.go
+++ lesson27/handson_01/main.go
@@ -3,7 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
+
+func main() {
+	r := httprouter.New()
+	r.GET("/", index)
+}
 
 func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Welcome!\n")
