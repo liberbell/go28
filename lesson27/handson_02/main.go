1dbc83407ed00f78f2c59b5d6211ec1dff3c2635
Liberbell <liberbell@gmail.com>
2024-09-19 08:10:56 +0900

    main1

--- lesson27/handson_02/main.go
+++ lesson27/handson_02/main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	s := `<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	<meta charset="utf-8">`
+}
