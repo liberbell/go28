c863c9726343590ff339dc2d598a2c17e6a5af92
Liberbell <liberbell@gmail.com>
2024-09-19 08:14:42 +0900

    main2

--- lesson27/handson_02/main.go
+++ lesson27/handson_02/main.go
@@ -10,5 +10,9 @@ func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	s := `<!DOCTYPE html>
 	<html lang="en">
 	<head>
-	<meta charset="utf-8">`
+	<meta charset="utf-8">
+	<title>Index</title>
+	</head>
+	<body>
+	<a href="`
 }
