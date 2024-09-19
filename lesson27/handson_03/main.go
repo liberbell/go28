90241d90878bc3564842c96485c3940d61a706b8
Liberbell <liberbell@gmail.com>
2024-09-20 08:30:47 +0900

    main1

---
+++ lesson27/handson_03/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	s := `<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	<meta charset="utf-8">
+	<title>Index</title>
+	</head>
+	<body>
+	<a href="/user/111222">Go to: http://localhost:8080/user/111222</a>
+	</body>
+	</html>`
+}
