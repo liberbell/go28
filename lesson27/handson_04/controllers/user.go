75cd7fb68b6a5baa46908892dd91bf6fe8769182
Liberbell <liberbell@gmail.com>
2024-09-21 10:20:02 +0900

    user3

--- lesson27/handson_04/controllers/user.go
+++ lesson27/handson_04/controllers/user.go
@@ -3,6 +3,8 @@ package controllers
 import (
 	"net/http"
 
+	"go28/lesson27/handson_04/models"
+
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -13,5 +15,10 @@ func NewUserController() *UserController {
 }
 
 func (uc UserController) GetUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-
+	u := models.User{
+		Name:   "James",
+		Gender: "Male",
+		Age:    45,
+		Id:     p.ByName("id"),
+	}
 }
