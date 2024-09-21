42f7787f37290548a17b6a798b70efd779e5639a
Liberbell <liberbell@gmail.com>
2024-09-21 10:11:50 +0900

    main1

---
+++ lesson27/handson_04/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"go28/lesson27/handson_03/models"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	r := httprouter.New()
+	r.GET("/", index)
+	r.GET("/user/:id", getUser)
+	r.POST("/user", createUser)
+	r.DELETE("/user/:id", deleteUser)
+	http.ListenAndServe(":8080", r)
+}
+
+func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	s := `<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	<meta charset="utf-8">
+	<title>Index</title>
+	</head>
+	<body>
+	<a href="/user/111222">Go to: http://localhost:8080/user/111222</a>
+	</body>
+	</html>`
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(s))
+}
+
+func getUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	u := models.User{
+		Name:   "James Bond",
+		Gender: "male",
+		Age:    32,
+		Id:     p.ByName("id"),
+	}
+
+	uj, _ := json.Marshal(u)
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, "%s\n", uj)
+}
+
+func createUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	u := models.User{}
+
+	json.NewDecoder(r.Body).Decode(&u)
+	u.Id = "007"
+	uj, _ := json.Marshal(u)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, "%s\n", uj)
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "Write code to delete user\n")
+}
