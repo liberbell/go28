81fab11c615977b80c5f32a57ade81ba1157f8c8
Liberbell <liberbell@gmail.com>
2024-09-21 10:16:28 +0900

    user2

--- lesson27/handson_04/controllers/user.go
+++ lesson27/handson_04/controllers/user.go
@@ -1,7 +1,17 @@
 package controllers
 
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
 type UserController struct{}
 
 func NewUserController() *UserController {
 	return &UserController{}
+}
+
+func (uc UserController) GetUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+
 }
