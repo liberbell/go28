5486035ae59345b219a84d747107ca5f9a394c12
Liberbell <liberbell@gmail.com>
2024-09-21 10:26:12 +0900

    user8

--- lesson27/handson_04/controllers/user.go
+++ lesson27/handson_04/controllers/user.go
@@ -24,7 +24,11 @@ func (uc UserController) GetUser(w http.ResponseWriter, r *http.Request, p httpr
 		Id:     p.ByName("id"),
 	}
 
-	uj, _ := json.Marshal(u)
+	uj, err := json.Marshal(u)
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "%s\n", uj)
@@ -41,7 +45,7 @@ func (uc UserController) CreateUser(w http.ResponseWriter, r *http.Request, _ ht
 	fmt.Fprintf(w, "%s\n", uj)
 }
 
-func (uc UserController) DeleteUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func (uc UserController) DeleteUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprint(w, "Write code to delete user\n")
 }
