4ff704646f365bf2d866b192c20ab423dc68b896
Liberbell <liberbell@gmail.com>
2024-09-22 15:31:30 +0900

    main2

--- lesson27/handson_05/main.go
+++ lesson27/handson_05/main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"context"
 	"go28/lesson27/handson_05/controllers"
+	"log"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"gopkg.in/mgo.v2"
 )
 
 func main() {
 	r := httprouter.New()
+	access_url := "mongodb+srv://<go_user>:<go_userpassword>@cluster0.4zuzwrb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0}"
+	clientOptions := options.Client().ApplyURI(access_url)
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
 	uc := controllers.NewUserController(getSession())
 	r.GET("/user/:id", uc.GetUser)
 	r.POST("/user", uc.CreateUser)
