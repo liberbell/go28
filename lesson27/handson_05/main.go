1fe84122f225de6bbe5b3b43575df2418586e459
Liberbell <liberbell@gmail.com>
2024-09-22 14:58:50 +0900

    main1

---
+++ lesson27/handson_05/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"go28/lesson27/handson_04/controllers"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	r := httprouter.New()
+	uc := controllers.NewUserController()
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+	http.ListenAndServe(":8080", r)
+}
