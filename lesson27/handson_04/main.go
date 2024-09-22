e66559ce14560f853f01d681f8cf4bf42f3d1af8
Liberbell <liberbell@gmail.com>
2024-09-22 14:41:32 +0900

    main1

--- lesson27/handson_04/main.go
+++ lesson27/handson_04/main.go
@@ -9,4 +9,7 @@ import (
 func main() {
 	r := httprouter.New()
 	uc := controllers.NewUserController()
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
 }
