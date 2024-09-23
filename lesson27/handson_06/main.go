0d2963140a4ad1febbe49c8ae761de42c3a516d9
Liberbell <liberbell@gmail.com>
2024-09-23 22:40:42 +0900

    main1

---
+++ lesson27/handson_06/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"go28/lesson27/handson_05/controllers"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+)
+
+func main() {
+	r := httprouter.New()
+
+	uc, err := controllers.NewUserController(getSession())
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+	http.ListenAndServe(":8080", r)
+}
+
+func getSession() (*mgo.Session, error) {
+	access_url := "mongodb+srv://<go_user>:<go_userpassword>@cluster0.4zuzwrb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0}"
+	s, err := mgo.Dial(access_url)
+	if err != nil {
+		panic(err)
+	}
+	return s, error
+}
